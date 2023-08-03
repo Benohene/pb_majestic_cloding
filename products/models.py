4bccdb977e20fed20c5103d607e4fe035eed9d3b
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-03 23:38:08 +0200

    Added product template and stock to product model

--- products/models.py
+++ products/models.py
@@ -18,22 +18,23 @@ class Category(models.Model):
 
 class Product(models.Model):
     SIZE_CHOICES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-        ('XL', 'Extra Large'),
-        ('XXL', 'Extra Extra Large'),
+        ('S', 'S'),
+        ('M', 'M'),
+        ('L', 'L'),
+        ('XL', 'XL'),
+        ('XXL', 'XXL'),
     )
     
     category = models.ForeignKey('Category', null=True,on_delete=models.CASCADE, blank=True)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    size = models.CharField(max_length=3, choices=SIZE_CHOICES, null=True, blank=True)
+    size = models.CharField(max_length=3, choices=SIZE_CHOICES, default='S')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    stock = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
+    ''' This function will render the products.html template '''
+    products = Product.objects.all()
 
-    return render(request, 'products/products.html')
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'products/products.html', context)
