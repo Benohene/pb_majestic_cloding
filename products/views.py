e71a53c0c3832dfc83fc1a4fb1048dbf982b11d1
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-04 12:41:27 +0200

    Added catergory search

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def all_products(request):
@@ -9,10 +9,15 @@ def all_products(request):
     ''' This function will render the products.html template '''
     products = Product.objects.all()
     query = None
-    
+    categories = None
     
     # Search functionality
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+            
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,7 +29,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        
+        'current_categories': categories,
     }
     
     return render(request, 'products/products.html', context)
