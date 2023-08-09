f4bc9506ba853410aa59bc235c5207ae08408b92
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-10 00:15:33 +0200

    Added product size logic to contexts file

--- cart/contexts.py
+++ cart/contexts.py
@@ -3,6 +3,12 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
+'''
+* is used to calculate the total amount of items in the cart, the total cost of the items in the cart, the delivery cost, and the grand total. 
+* It is then used in the cart view, and the add_to_cart view. 
+* The cart_contents function is also used in the context_processors.py file, which allows the cart to be available on every page of the site.
+'''
 def cart_contents(request):
     """ A view that renders the cart contents page """
     cart_items = []
@@ -10,18 +16,30 @@ def cart_contents(request):
     product_count = 0
     cart = request.session.get('cart', {})
     
-    for item_id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        cart_items.append({
-            'item_id': item_id, 
-            'quantity': quantity, 
-            'product': product
-            })
-    
-    
-    
+    # If the item has no size
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id, 
+                'quantity': item_data, 
+                'product': product
+                })
+        else:
+            # If the item has a size
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                cart_items.append({
+                    'item_id': item_id, 
+                    'quantity': quantity, 
+                    'product': product,
+                    'size': size,
+                    })
+                
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
