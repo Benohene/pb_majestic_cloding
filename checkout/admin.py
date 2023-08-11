f2fab88d6797acc2019ab70ec6a5ae89984a3c2b
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-11 15:55:11 +0200

    Added checkout forms, admin list and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    # This class is used to display the line items in the admin panel
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+    
+class OrderAdmin(admin.ModelAdmin):
+    # This class is used to display the order information in the admin panel
+    inlines = (OrderLineItemAdminInline,)
+    
+    readonly_fields = ('order_number', 'date', 'delivery_cost', 'order_total', 'grand_total',)
+    
+    fields = ('order_number', 'date', 'full_name', 'emal', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'region', 'delivery_cost', 'order_total', 'grand_total',)
+    
+    list_display = ('order_number', 'date', 'full_name', 'order_total', 'delivery_cost','grand_total',)
+    
+    ordering = ('-date',)
+    
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderLineItem)
--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+    
+    # Code taken from Code Institute Boutique Ado project
+    def ready(self):
+        import checkout.signals
