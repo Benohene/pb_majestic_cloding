1f76b3e3faba383b463d8484825e9d366fafd065
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-16 23:08:29 +0200

    Added order and profile pages in profile

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from .forms import OrderForm
 from products.models import Product
 from .models import OrderLineItem, Order
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from cart.contexts import cart_contents
 
 import stripe
@@ -123,6 +125,26 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+    
+    profile = UserProfile.objects.get(user=request.user)
+    order.user_profile = profile
+    order.save()
+    
+    if save_info:
+        profile_data = {
+            'default_full_name': order.full_name,
+            'default_phone_number': order.phone_number,
+            'default_street_address1': order.street_address1,
+            'default_street_address2': order.street_address2,
+            'default_town_or_city': order.town_or_city,
+            'default_postcode': order.postcode,
+            'default_region': order.region,
+            'default_country': order.country,
+        }
+        user_profile_form = UserProfileForm(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
+    
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from .models import UserProfile	
 from .forms import UserProfileForm
 from django.contrib import messages
+
+from checkout.models import Order
 
 # Create your views here.
 
@@ -19,12 +21,55 @@ def profile(request):
     
     # If the request is a POST request, get the form data
     form = UserProfileForm(request.POST or None, instance=profile)
-    order_history = profile.orders.all()
+    orders = profile.orders.all()
     
     template = 'profiles/profile.html'
     context = {
         'form': form,
-        'order_history': order_history,
+        'orders': orders,
+        'on_profile_page': True
+    }
+    
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """ A view to return the order history page """
+    order = get_object_or_404(Order, order_number=order_number)
+    
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+    
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True
+    }
+    
+    return render(request, template, context)
+
+
+def order_list(request):
+    """ A view to return the profile page """
+    # Get the user profile
+    profile = get_object_or_404(UserProfile, user=request.user)
+    
+    if request.method == 'POST':
+        # If the request is a POST request, get the form data
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+    
+    # If the request is a POST request, get the form data
+    form = UserProfileForm(request.POST or None, instance=profile)
+    orders = profile.orders.all()
+    
+    template = 'profiles/order_list.html'
+    context = {
+        'form': form,
+        'orders': orders,
         'on_profile_page': True
     }
     
