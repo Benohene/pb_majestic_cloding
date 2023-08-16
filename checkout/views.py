355cc9454bbaebd91009fb4eb60fa2f80f88c88f
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-16 23:39:32 +0200

    update checkout autofill with authenticated user

--- checkout/views.py
+++ checkout/views.py
@@ -103,7 +103,24 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
         
-        order_form = OrderForm()
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'town_or_city': profile.default_town_or_city,
+                    'postcode': profile.default_postcode,
+                    'region': profile.default_region,
+                    'country': profile.default_country,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
     
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
