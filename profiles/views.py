6c4369bb6de2d92ed259d9ba92fa97ee174b7808
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-16 02:57:33 +0200

    Added prfile forms, views and updated templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile	
+from .forms import UserProfileForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,9 +10,22 @@ def profile(request):
     # Get the user profile
     profile = get_object_or_404(UserProfile, user=request.user)
     
+    if request.method == 'POST':
+        # If the request is a POST request, get the form data
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+    
+    # If the request is a POST request, get the form data
+    form = UserProfileForm(request.POST or None, instance=profile)
+    order_history = profile.orders.all()
+    
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'order_history': order_history,
+        'on_profile_page': True
     }
     
     return render(request, template, context)
