a4619301c52515ce84877635bb9007849781d96e
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-19 01:46:38 +0200

    Added delete functionality

--- products/views.py
+++ products/views.py
@@ -84,9 +84,9 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             # This will save the form
-            form.save()
+            product=form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:	
@@ -119,3 +119,15 @@ def edit_product(request, product_id):
         'product': product,
     }
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """ A view to delete products from the store """
+    ''' This function will render the delete_product.html template '''
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
