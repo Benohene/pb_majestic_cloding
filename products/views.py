195e8adfd6a7e3b188c19cfcb4f0cd8f42275b38
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-19 01:01:09 +0200

    Added edit püroduct functionality

--- products/views.py
+++ products/views.py
@@ -96,3 +96,26 @@ def add_product(request):
         'form': form,
     }
     return render(request, template, context)
+
+def edit_product(request, product_id):
+    """ A view to edit products in the store """
+    ''' This function will render the edit_product.html template '''
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        # This will save the form
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:	
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+    return render(request, template, context)
