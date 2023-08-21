762b6e0706813b39ac7e887427428786cff036ab
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-21 19:44:05 +0200

    Setup AWS static and media for deployment

--- pd_majestic_cloding/settings.py
+++ pd_majestic_cloding/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     
     # Other
     'crispy_forms',
+    'storages',
     
 
 ]
@@ -185,6 +186,28 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Free shipping threshold
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+""" AWS S3 """
+# Bucket Config
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'pb-majestic-cloding'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
+    
+    # Static and media files
+    STATICFILES_LOCATION = 'static'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    # Media files
+    MEDIAFILES_LOCATION = 'media'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 # Stripe
 STRIPE_CURRENCY = 'eur'
