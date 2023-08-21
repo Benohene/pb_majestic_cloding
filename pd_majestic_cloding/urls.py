7cee9d4f2f94038b4ac0316bcdd7ff6ba03022cf
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-22 01:43:09 +0200

    added footer to pages

--- pd_majestic_cloding/urls.py
+++ pd_majestic_cloding/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import error_403, error_404, error_500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,8 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Error handling
+handler403 = 'pd_majestic_cloding.views.error_403'
+handler404 = 'pd_majestic_cloding.views.error_404'
+handler500 = 'pd_majestic_cloding.views.error_500'
