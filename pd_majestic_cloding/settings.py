5be0302c54619795a0468416b04148383231b7af
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-21 19:50:07 +0200

    Update AWS settings for deployment

--- pd_majestic_cloding/settings.py
+++ pd_majestic_cloding/settings.py
@@ -191,7 +191,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 # Bucket Config
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'pb-majestic-cloding'
-    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
