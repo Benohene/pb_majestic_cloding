5a1a6ea6647b8612d2d3602843963df95363a496
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-29 20:51:57 +0200

    Added 404 error handler 404

--- pd_majestic_cloding/urls.py
+++ pd_majestic_cloding/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -32,3 +33,7 @@ urlpatterns = [
     path("blog/", include("blog.urls")),
     path("contact/", include("contact.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# Error Handlers
+handler404 = 'pd_majestic_cloding.handler404'
--- pd_majestic_cloding/views.py
+++ pd_majestic_cloding/views.py
@@ -1,24 +1,7 @@
+'''This file is used to render the html pages'''
 from django.shortcuts import render
 
 
-def error_403(request, exception):
-    """ "
-    Handles HTTP 403 errors
-    """
-
-    return render(request, "errors/403.html")
-
-
-def error_404(request, exception):
-    """ "
-    Handles HTTP 404 errors
-    """
-
-    return render(request, "errors/404.html")
-
-
-def error_500(request,):
-    """ "
-    Handles HTTP 500 errors
-    """
-    return render(request, "errors/500.html")
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
