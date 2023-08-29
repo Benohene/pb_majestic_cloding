611c452da8f28393b50c67f44f78994506fafd2c
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-29 11:21:01 +0200

    removed secret key and set up debug

--- pd_majestic_cloding/settings.py
+++ pd_majestic_cloding/settings.py
@@ -21,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p!38e82^6)f3#20b65fipb^*gp^em(v*@krbmbtx4*90aa#l=*"
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['pbmajestic-cloding.herokuapp.com', 'localhost', '']
+ALLOWED_HOSTS = [
+    "pb-majestic-cloding-30c8cd3db475.herokuapp.com",
+    "pbmajestic-cloding.herokuapp.com",
+    "127.0.0.1",
+]
 CSRF_TRUSTED_ORIGINS = []
 
 # Application definition
