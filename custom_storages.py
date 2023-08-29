55360abf1794baa216aa33a864048d5203b1d304
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-29 07:35:10 +0200

    added the AWS buccket and storage configuration

--- custom_storages.py
+++ custom_storages.py
@@ -1,11 +1,15 @@
+""" This file is used to store static and media files in AWS S3 bucket"""
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    """ Store static files in AWS S3 bucket """
+    """Store static files in AWS S3 bucket"""
+
     location = settings.STATICFILES_LOCATION
 
+
 class MediaStorage(S3Boto3Storage):
-    """ Store media files in AWS S3 bucket """
+    """Store media files in AWS S3 bucket"""
+
     location = settings.MEDIAFILES_LOCATION
--- pd_majestic_cloding/settings.py
+++ pd_majestic_cloding/settings.py
@@ -13,9 +13,6 @@ import os
 from pathlib import Path
 import dj_database_url
 
-# import from env.py
-if os.path.exists("env.py"):
-    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
@@ -187,6 +184,25 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'pbmajestic-cloding'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    
+    # Static and Media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+    
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    
 
 # Free shipping threshold
 FREE_DELIVERY_THRESHOLD = 50
