1088eca50c43afc5f8115dfb52b4670bc41fb276
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-29 07:53:56 +0200

    Update deployment

--- pd_majestic_cloding/settings.py
+++ pd_majestic_cloding/settings.py
@@ -60,7 +60,6 @@ INSTALLED_APPS = [
     "contact",
     # Other
     "crispy_forms",
-    "django_countries",
     "django_resized",
     "storages",
     "django_summernote",
@@ -192,17 +191,16 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    
+
     # Static and Media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
-    
+
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
 
 # Free shipping threshold
 FREE_DELIVERY_THRESHOLD = 50
