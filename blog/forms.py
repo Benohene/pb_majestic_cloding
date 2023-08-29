e4ed8ef1acf063a6587b6b972503c7396f2f53a6
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-29 05:44:37 +0200

    Setup newsletter with Mailchimp

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-''' Blog form for admin to add/edit blog posts '''
+""" Blog form for admin to add/edit blog posts """
 from django_summernote.widgets import SummernoteWidget
 from django import forms
 from products.widgets import CustomClearableFileInput
@@ -24,40 +24,39 @@ class BlogForm(forms.ModelForm):
         ]
 
         widgets = {
-            "body": SummernoteWidget(attrs={'summernote': {'width': '100%', 'height': '400px'}}),
+            "body": SummernoteWidget(
+                attrs={"summernote": {"width": "100%", "height": "400px"}}
+            ),
             "image": CustomClearableFileInput(),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        """
-        Initializes the form attributes
-        """
         placeholders = {
             "title": "Enter Blog Title",
         }
         for field_name, field in self.fields.items():
-            field.widget.attrs["placeholder"] = placeholders.get(
-                field_name, ""
-            )
+            field.widget.attrs["placeholder"] = placeholders.get(field_name, "")
         self.fields["title"].widget.attrs["autofocus"] = True
 
 
 class CommentForm(forms.ModelForm):
-    ''' Form to add comments to blog posts '''
+    """Form to add comments to blog posts"""
 
     class Meta:
-        ''' Meta class to define model and fields to be used in form '''
+        """Meta class to define model and fields to be used in form"""
+
         model = Comment
-        fields = ('name', 'body')
+        fields = ("name", "body")
 
         labels = {
             "body": "Comment (Max 500 Characters) ",
         }
 
-
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['name'].widget.attrs['autofocus'] = True
+        self.fields["name"].widget.attrs["autofocus"] = True
+
+# black foramter command line code used to format this file:
+# black --exclude=env,venv --line-length=119 --target-version=py38 .
