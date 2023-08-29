3087cdf64be625af7ffdceebccd1f5486167bba7
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-29 06:15:40 +0200

    deploymet-adding database,disable collect statuic

--- pd_majestic_cloding/settings.py
+++ pd_majestic_cloding/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # import from env.py
 if os.path.exists("env.py"):
@@ -29,7 +30,7 @@ SECRET_KEY = "django-insecure-p!38e82^6)f3#20b65fipb^*gp^em(v*@krbmbtx4*90aa#l=*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pbmajestic-cloding.herokuapp.com', 'localhost', '']
 CSRF_TRUSTED_ORIGINS = []
 
 # Application definition
@@ -131,13 +132,18 @@ WSGI_APPLICATION = "pd_majestic_cloding.wsgi.application"
 
 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+# Database
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
