17161d5cb4c0d35ba7b113a1c83424baa47b74ef
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-08-29 10:01:27 +0200

    fixing static error for deployment commit

--- pd_majestic_cloding/urls.py
+++ pd_majestic_cloding/urls.py
@@ -18,23 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import error_403, error_404, error_500
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
-    path('products/', include('products.urls')),
-    path('cart/', include('cart.urls')),
-    path('checkout/', include('checkout.urls')),
-    path('profile/', include('profiles.urls')),
-    path('reviews/', include('reviews.urls')),
-    path('blog/', include('blog.urls')),
-    path('contact/', include('contact.urls')),
+    path("admin/", admin.site.urls),
+    path("summernote/", include("django_summernote.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("", include("home.urls")),
+    path("products/", include("products.urls")),
+    path("cart/", include("cart.urls")),
+    path("checkout/", include("checkout.urls")),
+    path("profile/", include("profiles.urls")),
+    path("reviews/", include("reviews.urls")),
+    path("blog/", include("blog.urls")),
+    path("contact/", include("contact.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Error handling
-handler403 = 'pd_majestic_cloding.views.error_403'
-handler404 = 'pd_majestic_cloding.views.error_404'
-handler500 = 'pd_majestic_cloding.views.error_500'
